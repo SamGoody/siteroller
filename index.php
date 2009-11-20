d0174817f641089bf838d45e6c5a32f9b112ef67
samgoody <ssgoodman@gmail.com>
2009-11-20 12:39:10 +0200

    Added proposed index.php
    Signed-off-by: samgoody <ssgoodman@gmail.com>

--- index.php
+++ index.php
@@ -1,1 +1,190 @@
-Nothing to see here, move on
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Siteroller Site builder, CMS, PHP/JS Library</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="l3.css" rel="stylesheet" type="text/css" />
+<link href="layout.css" rel="stylesheet" type="text/css" />
+<script src="maxheight.js" type="text/javascript"></script>
+</head>
+
+
+
+<body id="page1" onload="new ElementMaxHeight();">
+
+	<div id="stage">
+		<div id="header" style="height:auto;">
+			<div id="TRcorner"></div>
+			<div id="headMenu">
+				<div id="logo" style="font:11pt 'Comic Sans MS';">
+					<strong style="font:bold 14pt Georgia;">Siteroller</strong> 
+					<!--:Websites made <em>really</em> easy!!-->
+				</div>
+				
+				<span style="width:445px; position:relative; height:30px; vertical-align:bottom; line-height:10px; padding-top:20px;">
+					<img src="images/MenuTab.gif" id="menuBG" style="right:302px;" />
+					<a href="#" class="selected">Home</a><a href="projects">Projects</a><a href="services.php">Services</a><a href="blog">Blog</a><a href="contact.php">The Team</a>
+				</span> 
+			</div>
+			<hr >
+			
+			<div style="margin:0 8px; padding-bottom:4px; font:bold 42px 'Comic Sans MS'; color: #bbb; text-align:center; height:75px;">
+				Websites made <span style="font-style:italic; text-decoration:underline; color:#aaa">really</span> easy!
+			</div>
+			
+		</div>
+		<div id="body" style="border-top:solid 1px #DFD7CB;">
+			<style>
+				#iiimgs img.p3{top:13px; left:100px; width:auto; height:auto;} 
+				#iiimgs div{width:31%; height:225px; margin:1%; position:relative;} 
+				#iiimgs img{width:100%; height:225px; position:absolute; top:0; left:0} 
+				#iiimgs div.desc,#iiimgs a{left:0; text-align:center; position:absolute; width:100%; color:#2E1302}
+				
+				.td{ display:inline-block; vertical-align:top; }
+				div.td{_display:inline;}
+			</style>
+			<div id="iiimgs" style="position:relative; width:100%;">
+				<div class="td">
+					<img  alt="" src="images3/bann3Clean3.jpg" class="pic2" />
+					<img alt="" style="left:50%; margin-left:-24px; height:48px"src="images3/desktop.png" class="p3" />
+					<a style="top:85px; font:bold 26px Helvetica;">SiteBuilder &amp; CMS</a>
+					<div class="desc" style="top:116px;  text-align:justify; font:bold 13px Helvetica"><div style="padding:10px; width:auto; height:auto; font-style:italic">Free site builder & CMS for even complete beginners to build and maintain a website, on any host [<a href="#">?</a>]</div></div>
+					<a style="bottom:14px; font:bold 12px Helvetica">Learn More</a>
+				</div><div class="td">
+					<img alt="" src="images3/bann2Clean3.jpg" />
+					<img alt="" style="left:50%; margin-left:-24px;"src="images3/Siconthemes.png" was="background.png" class="p3" />
+					<a style="top:85px; font:bold 26px Helvetica;">Hosting</a>
+					<a style="top:118px;  text-align:justify; font:bold 14px Helvetica">
+						<div style="padding:10px;width:auto; height:auto;font-style:italic">
+							Hosting with the CMS / Sitebuilder setup and waiting.<br>Be up &amp; running in 10 min.
+						</div>
+					</a>
+					<a style="bottom:14px; font:bold 12px Helvetica">Learn More</a>
+				</div><div class="td">
+					<img  alt="" src="images3/bann4Clean3.jpg" />
+					<img alt="" style="left:50%; margin-left:-24px;"src="images3/kdict.png" class="p3" />
+					<a style="top:85px; font:bold 26px Helvetica;">The Library</a>
+					<a style="top:118px;  text-align:justify; font:bold 14px Helvetica">
+						<div style="padding:10px;width:auto; height:auto;font-style:italic">
+							PHP Classes <br>Documented, sorted, rated.<br> Take your pick.
+						</div>
+					</a>
+					<a style="bottom:14px; font:bold 12px Helvetica">Learn More</a>
+				</div>
+			</div>
+			
+			<div style="padding-top:50px;">
+				<div class="td" style="width:49%; position:relative;">
+					<div style="height:50px; margin-top:25px; font:24px Papyrus; text-align:center;">The webcast</div>
+					<div style="margin-left:15px">
+						<object width="100%" height="250">
+							<param name="allowfullscreen" value="true" />
+							<param name="allowscriptaccess" value="always" />
+							<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=2909565&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" />
+							<embed src="http://vimeo.com/moogaloop.swf?clip_id=2909565&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="100%" height="300"></embed>
+						</object>
+					</div>
+				</div>
+				<div class="td" style="width:50%">
+					<fieldset style="margin:15px;padding:15px; font:bold 12px Helvetica; -moz-border-radius: 7px;">
+						<img src="images3/kwikdisk.pngx" style="position:absolute; top:0; right:0;position:relative;" >
+						<legend>Beginner? We've got an easy site building tool</legend>
+						Need a website, but don't know <i>anything</i> about webdesign or coding?<br>
+						With the Siteroller Builder even the most novice user can create a whole website in no time flat!<br>
+						If you can read English and can click a mouse, you can make a blog or ecommerce site.<br>
+						<br>
+						CMS stands for "content management sytem" - fancy words that mean software to build or edit web pages.<br>
+						For simplicity, we refer to the fancy point-and-click tool as the Builder, and the advanced coding tools as the CMS.<br>
+						But don't fret the jargon, download and start playing - it's free and easy!
+					</fieldset>
+					<fieldset style="margin:15px;padding:15px; font:bold 12px Helvetica; -moz-border-radius: 7px;">
+						<legend>Expert Coder? We've got a CMS worth lookin' at.</legend>
+						<!--
+						The "large" CMSs (Drupal, Joomla, ModX, Wordpress, etc.) are like programming languages - that are heavy things that will take you wekMost Site Building
+						
+						The Siteroller CMS is a complete plug and play sitebuilding tool.<br>
+						Super simple to setup and run, powerful enough to run full fledged ecommerce platforms.<br>
+						Powerful: Siteroller's templating system allows for everything you'd expect from a world class CMS. 
+						Unobtrusive: Siteroller does not interfere with your code or impose any one IDE, structure, or MVC pattern.<br> 
+						Easy: Consisting of just a few powerful commands, you can tap into most functionality with a minutes. <br>
+						Quick: No database needed, no fancy permissions, just upload and run.
+						Super simple to setup and run, powerful enough to run full fledged ecommerce platforms.<br>
+						-->
+						A tiny but powerful CMS that doesn't require any changes to your current structure or coding style.<br>
+						Set up in minutes, take under 10 minutes to learn, deploy anywhere (doesn't even require a database!).<br>
+<br>
+						Think of Siteroller as a dab a dab of crazy glue - a few tiny classes which allow your existing scripts to call a whole array of generic PHP and Javascript functions.<br>
+						<br>
+						Want to add an element from page A to page B?
+							<div style="background-color:#E7E7DE; padding-left:35px">&lt;include page="A" section=&quot;myDiv&quot; /&gt;</div>
+						Need a Javascript form validator? 
+							<div style="background-color:#E7E7DE; padding-left:35px">&lt;form id=&quot;myForm&quot; class=&quot;sr_formvalidator&quot;&gt;</div>
+					</fieldset>
+				</div>
+			</div>
+		</div>
+	</div>	
+	<div id="footer">
+		<a href="contact.php">Contact</a>|<a href="http://siteroller.net/services">Design by Siteroller</a>|<a href="http://www.siteroller.net">Powerd by Siteroller</a>
+	</div>
+<!--
+	<div id="site_center">
+		<div id="main">
+			
+			<div id="content">
+				<div class="line_bottom">
+					<div class="l_b">
+						<div class="r_b">
+							<a href="#"><img alt="" src="images/bann1.jpg" class="pic1" /></a><a href="#"><img alt="" src="images/bann2.jpg" class="pic3" /></a><a href="#"><img alt="" src="images/bann3.jpg" class="pic2" /></a><a href="#"><img alt="" src="images/bann4.jpg" class="pic3" /></a><a href="#"><img alt="" src="images/bann5.jpg" class="pic2" /></a>
+							<div class="bg_line">
+								<div class="ind">
+									<img alt="" src="images/1_w1.gif" class="title" /><br />
+									<b>Lorem ipsum dolor</b> sit ametco nse ctetuer adeugiat malesuada odio. Morbi nunc odio, gravida at, cursus nectus aaecenas tristue orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. onec sit amet eros. Lorem ipsum dolor sit amtuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum ves eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacutibulum libero nislcelerisque eget, malesuada at, neque. Vivamus eget nibh. Etiam cursus leo vel metus.
+							  </div>
+							</div>
+							<div class="box">
+								<div class="pad">
+										<div class="column1 maxheight">
+											<div class="ind1">
+												<img alt="" src="images/1_w2.gif" class="title" />
+												<p><b>12 04 2008</b> sit ametco nse ctetuer adugiat male<br /> suada odio. Morbi nunc odioravida at, cursus nec tus aaecenas tristue orcltricieph...&nbsp; &nbsp; <a href="#">more</a></p>			
+												<b>12 04 2008 sit</b> ametco nse ctetuer adeugiat male suada odio. Morbi nunc odio, gravida sus nec tus aaecenas tristue orcltricieph...&nbsp; &nbsp; <a href="#">more</a>
+										  </div>	
+										</div>
+										<div class="column2 maxheight">
+											<div class="ind1">
+												<img alt="" src="images/1_w3.gif" class="title" />
+												<ul>
+													<li><a href="#">Sit ametco nse ctetuer adugiat male suada odirbi nunc odolo</a></li>
+													<li><a href="#">Mauris fermentum dictum magna. Sed laoreet aliquam leo</a></li>
+													<li><a href="#">Ut tellus dolor, dapibus eget, elementum ves eleifendelit</a></li>
+													<li><a href="#">Aenean auctor wisi et urna. Aliquam erat volutpat</a></li>
+													<li><a href="#">Duis ac turpis. Integer rutrum ante eu lacutibulum libero</a></li>
+													<li><a href="#">Vivamus eget nibh. Etiam cursus leo vel metulla faenean nec </a></li>
+												</ul>
+										  </div>
+									</div>
+									<div class="clear"></div>
+					  		  </div>
+				  		  </div>
+					  </div>
+				  </div>
+			  </div>
+  		  </div> -->
+			<!-- footer --><!--
+			<div id="footer">
+				<div class="box">
+					<div class="column1">
+						Digitbizz &copy; 2008  &bull; <a href="index-7.html">Privacy Policy</a>
+					</div>
+					<div class="column2">
+						<a href="#">RSS</a> <a href="#"><img alt="" src="images/z_footer.jpg" class="pic1" /></a>					
+					</div>
+					<div class="clear"></div>
+				</div>			
+			</div>
+		</div>
+	</div>
+-->	
+</body>
+</html>
